2376ee4f1b1f03dd882519b789998a6f78808fd6
Yogesh Jamdade <yjamdade49@gmail.com>
2026-07-10 12:13:19 +0530

    feat added send mail functionality

---
+++ packages/cli/src/gmail/send.ts
@@ -0,0 +1,37 @@
+import { gmail } from "./client";
+
+interface SendEmailOptions {
+  to: string;
+  subject: string;
+  body: string;
+}
+
+export async function sendEmail({
+  to,
+  subject,
+  body,
+}: SendEmailOptions) {
+  const message = [
+    `To: ${to}`,
+    "Content-Type: text/plain; charset=utf-8",
+    "MIME-Version: 1.0",
+    `Subject: ${subject}`,
+    "",
+    body,
+  ].join("\r\n");
+
+  const encodedMessage = Buffer.from(message)
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+
+  const res = await gmail.users.messages.send({
+    userId: "me",
+    requestBody: {
+      raw: encodedMessage,
+    },
+  });
+
+  return res.data;
+}
--- packages/cli/src/test.ts
+++ packages/cli/src/test.ts
@@ -1,6 +1,12 @@
 import { gmail } from "./gmail/client";
 import { listEmails } from "./gmail/list";
 
-const emails = await listEmails();
+import { sendEmail } from "./gmail/send";
 
-console.log(emails);
+const result = await sendEmail({
+  to: "jamdadeyogesh40@gmail.com",
+  subject: "Hello from MailKit",
+  body: "This email was sent using the Gmail API.",
+});
+
+console.log(result);
